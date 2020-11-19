018b5b0ed12ceccfc225e97f0f44b5c213aa4b98
Edison Neto <ednetoali@gmail.com>
2020-11-19 10:08:48 -0300

    Add endpoint to redirect to original URL

--- src/main/java/org/eldron/shorty/controller/UrlController.java
+++ src/main/java/org/eldron/shorty/controller/UrlController.java
@@ -3,17 +3,16 @@ package org.eldron.shorty.controller;
 import org.eldron.shorty.service.UrlService;
 import org.eldron.shorty.vo.Url;
 import org.eldron.shorty.vo.request.ShortenUrlRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/shorten")
 public class UrlController {
     private final UrlService urlService;
 
@@ -22,12 +21,20 @@ public class UrlController {
     }
 
     @GetMapping("/{shortenedUrlId}")
+    public ResponseEntity<Void> redirectToOriginalUrl(@PathVariable("shortenedUrlId") final String shortenedUrlId) {
+        final var url = urlService.getOriginalUrl(shortenedUrlId);
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header(HttpHeaders.LOCATION, url.getUrl())
+                .build();
+    }
+
+    @GetMapping("/shorten/{shortenedUrlId}")
     public ResponseEntity<Url> getOriginalUrl(@PathVariable("shortenedUrlId") final String shortenedUrlId) {
         final var url = urlService.getOriginalUrl(shortenedUrlId);
         return ResponseEntity.status(HttpStatus.OK).body(url);
     }
 
-    @PostMapping
+    @PostMapping("/shorten")
     public ResponseEntity<Url> shortenUrl(@RequestBody final ShortenUrlRequest shortenUrlRequest) {
         final var shortenedUrl = urlService.shortenUrl(shortenUrlRequest.getUrl());
         return ResponseEntity.status(HttpStatus.CREATED).body(shortenedUrl);
